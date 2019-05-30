0d30b81fb93d4363603f78bab86fcfb05fcecf25
Juliano Custódio <juliano.custodio@hotmail.com.br>
2019-05-30 14:35:16 -0300

    implements the example of how to use ML.NET in Xamarin.Forms Applications

--- DemoCommentary/MainPage.xaml.cs
+++ DemoCommentary/MainPage.xaml.cs
@@ -1,21 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace DemoCommentary
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        public async void GetResult(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(Texto.Text))
+                return;
+
+            using (var client = new HttpClient())
+            {
+                client.Timeout = new TimeSpan(0, 0, 0, 30);
+
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("http://192.168.0.5:5000/api/commentary/" + Texto.Text),
+                    Method = HttpMethod.Get
+                };
+
+                var result = await client.SendAsync(request);
+
+                if (!result.IsSuccessStatusCode)
+                    throw new Exception(result.Content.ReadAsStringAsync().Result);
+
+                var json = result.Content.ReadAsStringAsync().Result;
+                var data = JsonConvert.DeserializeObject<PredictionResult>(json);
+
+                
+                if (data.Predicition)
+                    Emoji.Source = "happy.png";
+                else
+                    Emoji.Source = "sad.png";
+
+                LblResult.Text = $"Predicition:{data.Predicition}\nScore:{data.Score}\nProbability: {data.Probability}";
+
+            }
         }
     }
 }
---
+++ DemoCommentary/PredictionResult.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoCommentary
+{
+    public class PredictionResult
+    {
+        public bool Predicition { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
